4ecd98bd27d4ba528b368bbc8c37593c8c65a12a
Kub1991 <borisyakszy@gmail.com>
2025-07-03 00:44:47 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -250,7 +250,19 @@ function App() {
         </div>
       </div>
       <MoodSection onMoodClick={handleMoodClick} />
-      <RegistrationSection onRegistrationSuccess={handleLoginSuccess} />
+      <div className="py-8 bg-[#070000]">
+        <div className="max-w-6xl mx-auto px-6">
+          <div 
+            className="h-px w-full"
+            style={{
+              background: 'linear-gradient(90deg, transparent, rgba(223, 189, 105, 0.3), transparent)'
+            }}
+          ></div>
+        </div>
+      </div>
+      <div className="bg-[#070000]">
+        <RegistrationSection onRegistrationSuccess={handleLoginSuccess} />
+      </div>
       <Footer />
       
       <AuthPromptModal 
